49c259461966baf662ad98dd2e04c7540f8bfacc
Phoenix681 <mayank.mittal2504@gmail.com>
2024-07-15 20:47:00 +0530

    Day-1 and 2 Added

---
+++ Day-1/Variable-and-Datatypes.js
@@ -0,0 +1,42 @@
+// Variable Declaration
+
+var n = 15;
+console.log(n);
+
+let s = "Hello World";
+console.log(s);
+
+// Constant Declaration
+
+const f = true;
+console.log(f);
+
+// var vs let vs const 
+// var can be updated and redeclared  
+// let can be updated but not redeclared
+// const can neither be updated nor redeclared
+
+// Data Types
+
+var a = 6;
+console.log(a,typeof(a))
+var b = 'Goodbye';
+console.log(b,typeof(b))
+var c = false;
+console.log(c,typeof(c))
+var d = {
+    Food : 'ramen',
+    Place : 'Japan',
+    Rating: 8
+};
+console.log(d,typeof(d))
+var e = ['sky',84,'red'];
+console.log(e,typeof(e))
+
+// Reassigining Variables
+let p = 96;
+console.log(p)
+
+p = 69;
+console.log(p)
+
---
+++ Day-2/Operator.js
@@ -0,0 +1,44 @@
+// Arithmetic Operator
+var a = 5+4;
+console.log(a);
+var b = 5-4;
+console.log(b);
+var c = 5/4;
+console.log(c);
+var d = 5*4;
+console.log(d);
+var e = 5%4;
+console.log(e);
+
+// Assignment Operator
+
+var f = 2;
+f+=5;
+console.log(f);
+
+var g = 3;
+g-=8;
+console.log(g);
+
+// Comparison Operator
+
+console.log(a<b);
+console.log(a>=g);
+console.log(b===e)
+console.log(b==='1')
+console.log(b=='1')
+
+// Logical Operator
+
+var p = 5>8;
+var q = 6<=7;
+var r = 1==1;
+console.log(p && q);
+console.log(q || p);
+console.log(!r)
+
+// Ternary Operator
+
+var x = -9;
+var t = x>=0 ? 'Positive':'Negative';
+console.log(t)
